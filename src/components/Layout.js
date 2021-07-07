03694ce3be94082c5edb11af08c1125470c699fb
Mason <xxninjabunnyxx@gmail.com>
2021-07-06 20:22:01 -0400

    added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar'
 const Layout = ({ children }) => {
     return (
         <>
+        <Navbar />
         <div className={"max-w-screen-md mx-auto p-1"}>{children}</div>
         </>
     )
