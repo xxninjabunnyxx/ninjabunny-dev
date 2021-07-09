ce527ff1c308a0f087aac94478084c18cc022e7b
Mason <xxninjabunnyxx@gmail.com>
2021-07-09 17:43:04 -0400

    added layout

--- src/templates/Page.js
+++ src/templates/Page.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import Layout from '../components/Layout'
 
 const PageTemplate = ({ data }) => {
   const post = data.mdx;
 
   return (
-    <>
+    <Layout>
       <div>{post.frontmatter.title}</div>
       <div>{post.frontmatter.date}</div>
       <MDXRenderer>{post.body}</MDXRenderer>
-    </>
+      </Layout>
   );
 };
 
