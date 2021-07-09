4e8ba3620a1c63237721fc225235591360591fcc
Mason <xxninjabunnyxx@gmail.com>
2021-07-09 17:42:41 -0400

    added layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Navbar from '../components/Navbar'
+import Layout from '../components/Layout'
 
 const IndexPage = () => {
   return (
-    <Navbar>
+    <Layout>
       homepage
-    </Navbar>
+    </Layout>
   )
 }
 
