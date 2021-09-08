ba367cdd555fddee3a867336b80824dde4576d51
Mason <xxninjabunnyxx@gmail.com>
2021-09-08 16:05:36 -0400

    removed

--- cypress/e2e/generated/homePage.spec.js
+++
@@ -1,9 +0,0 @@
-describe("Test Home Page", () => {
-  it("Load the Home page", () => {
-    cy.visit("/")
-  })
-
-  it("Should have logo text.", () => {
-    cy.get('[cy-data="logo-text"]').should("exist")
-  })
-})
