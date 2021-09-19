751097193b0ac3463d37b0f2b7ba48d41a64e571
Mason <xxninjabunnyxx@gmail.com>
2021-09-19 09:00:19 -0400

    made placeholder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 //import { Link, graphql } from "gatsby"
+import styled from 'styled-components'
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
@@ -8,9 +9,15 @@ const HomePage: React.FC = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      home
+      <PlaceholderStyle>
+        <div>Coming Soon</div>
+      </PlaceholderStyle>
     </Layout>
   )
 }
 
+const PlaceholderStyle = styled.div`
+display: flex;
+justify-content: center;
+`
 export default HomePage
